afbaa2a4f345f65a528cf4e4ef7fb4defdfb3c3e
AntonOshurek <oshurekweb@gmail.com>
2021-09-20 13:14:31 +0200

    new es6 modules)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,22 @@
-const hello = require('./block-1');
+//old
+//const hello = require('./block-1');
+//hello();
 
-hello();
+
+//es 6 standart!
+
+import {foo, bar} from './block-1';
+console.log(`${foo} ${bar}`);
+
+//default
+import saydefault from './block-1';
+saydefault();
+
+//how renames inport variables or functions or some one)
+import {rename as ren} from './block-1';
+console.log(ren);
+
+//import all functions and variables of js file
+import * as data from './block-1';
+console.log(`${data.foo} or ${data.bar}`); //we get an object and work with it as an object
+data.saysome();
